886f655ed40a99d72407d4b892a97bc371c1dce9
Bissbert <43237892+Bissbert@users.noreply.github.com>
2025-09-15 15:31:41 +0200

    removed '0' file

--- config.sh
+++ config.sh
@@ -1,8 +1,3 @@
-# >>> zbx-config overrides >>>
-export ZABBIX_URL="https://zabbix.kwo.ch/zabbix/api_jsonrpc.php"
-export ZABBIX_API_TOKEN="7accd491d2fb6738981e8df1fe886d159fb366c8608bbc72c1401b8eaf85b5c8"
-# <<< zbx-config overrides <<<
-
 # zbx-toolkit config
 : "${ZABBIX_URL:=https://zabbix.example.com/api_jsonrpc.php}"
 : "${ZABBIX_USER:=apiuser}"
